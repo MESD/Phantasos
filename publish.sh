2a146fc8f1dbfe499dd55ea1700c4d2ef171e87e
Tyler Hand <thand@mesd.k12.or.us>
2015-04-29 14:00:40 -0700

    publish script

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -f /var/www/phantasos/app/config/parameters.yml ]
+then
+    cp /var/www/phantasos/app/config/parameters.yml app/config/
+fi
+if [ -f /var/www/phantasos/version.txt ]
+then
+    cp /var/www/phantasos/version.txt .
+fi
+a=`git log -1 --format="%H"`
+b=`date`
+c=`whoami`
+echo "$a $b $c" >> version.txt
+composer install
+php app/console assets:install
+php app/console assetic:dump
+rm -rf app/cache/*
+rm -rf app/logs/*
+rm -rf app/sessions/*
+sudo chown -R apache:appdevs .
+sudo rsync -az --delete --exclude-from exclude.txt . /var/www/phantasos/
+sudo chown -R `whoami`:`whoami` .
+sudo systemctl reload httpd.service
